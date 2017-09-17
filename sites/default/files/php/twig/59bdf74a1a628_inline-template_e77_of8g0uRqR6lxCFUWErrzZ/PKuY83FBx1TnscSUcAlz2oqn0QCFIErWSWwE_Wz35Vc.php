e6074686984f9cdbc839226248612f9041fee47c
j-paterson <jesse.r.pat@gmail.com>
2017-09-16 21:38:29 -0700

    Added composer, working on drush

---
+++ sites/default/files/php/twig/59bdf74a1a628_inline-template_e77_of8g0uRqR6lxCFUWErrzZ/PKuY83FBx1TnscSUcAlz2oqn0QCFIErWSWwE_Wz35Vc.php
@@ -0,0 +1,75 @@
+<?php
+
+/* {# inline_template_start #}<div style="float:left; margin:20px;">{{ field_banner }}</div> */
+class __TwigTemplate_e5811b7f5f71f51a918ad477cc4d44c134ac3e9c4d48773ba968ee714b33ce2e extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array();
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('Twig_Extension_Sandbox')->checkSecurity(
+                array(),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 1
+        echo "<div style=\"float:left; margin:20px;\">";
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, (isset($context["field_banner"]) ? $context["field_banner"] : null), "html", null, true));
+        echo "</div>";
+    }
+
+    public function getTemplateName()
+    {
+        return "{# inline_template_start #}<div style=\"float:left; margin:20px;\">{{ field_banner }}</div>";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  43 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("", "{# inline_template_start #}<div style=\"float:left; margin:20px;\">{{ field_banner }}</div>", "");
+    }
+}
